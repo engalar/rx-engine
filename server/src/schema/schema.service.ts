b710e6f035e7eab332fa09ceacb025838015b9de
Water.Li <lizongbin@hotmail.com>
2022-01-27 21:19:38 +0800

    add lodash

--- server/src/schema/schema.service.ts
+++ server/src/schema/schema.service.ts
@@ -1,6 +1,7 @@
 import { buildSchema, GraphQLSchema } from 'graphql';
 import { EntityType } from '../meta/meta-interface/entity-meta';
 import { MetaService } from '../meta/meta.service';
+import _ = require('lodash');
 
 export class SchemaService {
   constructor(protected metaService: MetaService) {}
@@ -19,7 +20,7 @@ export class SchemaService {
           queryString =
             queryString +
             `
-          ${entityMeta.name}: ${entityMeta.name}
+          ${_.lowerFirst(entityMeta.name)}: ${entityMeta.name}
           
         `;
 
